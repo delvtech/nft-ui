a11dd00e3e7b38644568787d55fa538c00405831
Tina Haibodi <tinahaibodi96@gmail.com>
2022-02-07 07:18:47 +0100

    adding regenerated section (#7)

---
+++ components/Regenerated/index.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import styled from "styled-components";
+import { SectionContainer } from "common/Container";
+
+import Thumbnail from "public/assets/png/Thumbnail.png";
+
+export const Regenerated = () => {
+  return (
+    <SectionContainer>
+      <h2>regenerated uniquely</h2>
+      <ImageContainer>
+        <Image src={Thumbnail} layout="fill" alt="regenerated uniquely" />
+      </ImageContainer>
+    </SectionContainer>
+  );
+};
+
+const ImageContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: calc(100vw / 1.7);
+  max-height: 725px;
+`;
