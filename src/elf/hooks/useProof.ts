77ddd98fd242af126b5baf9e31e807aa2969ea38
Cash DeLeon <cashd@users.noreply.github.com>
2022-02-23 14:58:04 -0700

    Update useProof.ts

--- src/elf/hooks/useProof.ts
+++ src/elf/hooks/useProof.ts
@@ -2,20 +2,22 @@ import { BigNumber, BytesLike } from "ethers";
 import { useQuery } from "react-query";
 
 interface ProofData {
-  tokenId: BigNumber;
-  proof: BytesLike[];
+  tokenId?: BigNumber;
+  proof?: BytesLike[];
 }
+
+const fakeProof = {
+  tokenId: BigNumber.from(0),
+  proof: [
+    "0x3f68e79174daf15b50e15833babc8eb7743e730bb9606f922c48e95314c3905c",
+    "0x31403139b3e90fd160d993560f6de598174a3c5cbb1dd8614454219f590c1d57",
+    "0x2c70357a690b685ecef91703e08c73f3be5a8fe584b65cb1de18eb9dd3308dcd",
+  ],
+};
 
 // Mocked data for now
 export const useProof = (address: string | undefined | null) => {
   return useQuery<ProofData>(["nft-proof", address], () => {
-    return Promise.resolve({
-      tokenId: BigNumber.from(0),
-      proof: [
-        "0x3f68e79174daf15b50e15833babc8eb7743e730bb9606f922c48e95314c3905c",
-        "0x31403139b3e90fd160d993560f6de598174a3c5cbb1dd8614454219f590c1d57",
-        "0x2c70357a690b685ecef91703e08c73f3be5a8fe584b65cb1de18eb9dd3308dcd",
-      ],
-    });
+    return Promise.resolve(fakeProof);
   });
 };
