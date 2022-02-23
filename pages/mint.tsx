be35e9793262be628f4dde01fd1d311864369e1a
Cash DeLeon <cashd@users.noreply.github.com>
2022-02-23 14:27:36 -0700

    show mint placeholder if not eligable for mint

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -1,9 +1,17 @@
+import { MintPlaceholder } from "components/Mint/MintPlaceholder";
+import { useProof } from "elf/hooks/useProof";
+import useWeb3 from "elf/useWeb3";
 import { NextPage } from "next";
 import React from "react";
 import { Mint } from "../components/Mint";
 
 const MintPage: NextPage = () => {
-  return <Mint />;
+  const { account } = useWeb3();
+  const { data: proofData } = useProof(account);
+
+  const isAllowlisted = proofData?.proof && proofData.tokenId;
+
+  return isAllowlisted ? <Mint /> : <MintPlaceholder />;
 };
 
 export default MintPage;
