662486e289c0824d46f47c195207ddc798989606
Cash DeLeon <cashd@users.noreply.github.com>
2022-03-02 23:57:56 -0600

    updated useProof to use data from s3 or local mock

--- src/elf/hooks/useProof.ts
+++ src/elf/hooks/useProof.ts
@@ -1,38 +1,35 @@
 import axios from "axios";
-import { BigNumber, BytesLike } from "ethers";
+import useWeb3 from "elf/useWeb3";
+import { ChainId } from "elf/wallets/chains";
 import { useQuery } from "react-query";
-
-type TokenId = BigNumber;
-type Proof = BytesLike[];
-type Leaf = {
-  address: string;
-  tokenId: TokenId;
-};
-type ProofData = { leaf: Leaf; proof: Proof };
-type ProofDataResponse = Array<ProofData>;
-
-// const fakeProof = {
-//   tokenId: BigNumber.from(0),
-//   proof: [
-//     "0x3f68e79174daf15b50e15833babc8eb7743e730bb9606f922c48e95314c3905c",
-//     "0x31403139b3e90fd160d993560f6de598174a3c5cbb1dd8614454219f590c1d57",
-//     "0x2c70357a690b685ecef91703e08c73f3be5a8fe584b65cb1de18eb9dd3308dcd",
-//   ],
-// };
+import { ProofData, ProofDataResponse } from "src/types";
 
 const S3_BUCKET_URI = "https://elementfi.s3.us-east-2.amazonaws.com/nft";
 
-const getProofURI = (address: string) => `${S3_BUCKET_URI}/goerli2/${address}`;
+const getProofURI = (address: string, chainId?: number) => {
+  if (chainId === ChainId.GOERLI) {
+    return `${S3_BUCKET_URI}/goerli2/${address}`;
+  }
+
+  if (chainId === ChainId.MAINNET) {
+    // TODO @cashd: update when mainnet bucket is ready
+    return `${S3_BUCKET_URI}/goerli2/${address}`;
+  }
+
+  // defaulting to local testnet, fetching proof from public/proofs/<address>
+  // see available proofs for correct wallet to use for testing
+  return `/proofs/${address}.json`;
+};
 
 export const useProof = (address: string | undefined | null) => {
+  const { chainId } = useWeb3();
   return useQuery<ProofData | undefined>(["nft-proof", address], async () => {
     if (!address) {
       return undefined;
     }
-    // console.log(getProofURI(address));
-    const { data } = await axios.get<ProofDataResponse>(getProofURI(address));
-
-    console.log(data);
+    const { data } = await axios.get<ProofDataResponse>(
+      getProofURI(address, chainId),
+    );
 
     if (data && data[0]) {
       return data[0];
