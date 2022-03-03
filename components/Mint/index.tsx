bcb3f9268037796b2e6169b340098b4bfcc686f5
Cash DeLeon <cashd@users.noreply.github.com>
2022-03-02 20:59:16 -0600

    init proof fetcher

--- components/Mint/index.tsx
+++ components/Mint/index.tsx
@@ -1,23 +1,26 @@
-import Image from "next/image";
-import MintGIF from "public/assets/gif/hero_image.gif";
-import ReactTextTransition, { presets } from "react-text-transition";
-import content from "./content.json";
-import useWeb3 from "elf/useWeb3";
+import { PrimaryButton } from "common/Button/styles";
 import { ContentPage } from "components/ContentPage";
 import { ContentWrapper } from "components/Entrance/styles";
-import { Fade } from "react-awesome-reveal";
 import { MintContainer, ProgressContainer } from "components/Mint/styles";
-import { PrimaryButton } from "common/Button/styles";
-import { useEffect, useMemo, useState } from "react";
+import { useMinter } from "elf/hooks/useMinter";
 import { useProof } from "elf/hooks/useProof";
 import { useWalletDialog } from "elf/hooks/useWalletDialog";
+import useWeb3 from "elf/useWeb3";
+import Image from "next/image";
+import MintGIF from "public/assets/gif/hero_image.gif";
+import { useEffect, useMemo, useState } from "react";
+import { Fade } from "react-awesome-reveal";
+import ReactTextTransition, { presets } from "react-text-transition";
+import content from "./content.json";
 
 const UPDATE_PER = 100;
 
 export const Mint = () => {
-  const { active, account } = useWeb3();
+  const { active, account, library } = useWeb3();
   const { data: proofData } = useProof(account);
   const { openModal } = useWalletDialog();
+
+  const { mutate: mint } = useMinter(library?.getSigner());
 
   const [seconds, setSeconds] = useState<number>(1);
   const [showProgress, setShowProgress] = useState<boolean>(false);
@@ -42,12 +45,15 @@ export const Mint = () => {
     return () => clearInterval(timer);
   }, [seconds, showProgress]);
 
-  const isAllowListed = useMemo(
-    () => proofData?.proof && proofData?.tokenId,
-    [proofData],
-  );
+  const canMint = useMemo(() => !!proofData, [proofData]);
 
-  const handleClick = () => setShowProgress(true);
+  const handleMint = () => {
+    // setShowProgress(true);
+
+    if (canMint && proofData) {
+      mint([proofData.leaf.tokenId, proofData.proof]);
+    }
+  };
 
   return (
     <ContentPage padding="100px 124px 144px 124px" title="Mint">
@@ -62,16 +68,14 @@ export const Mint = () => {
         {!showProgress ? (
           <>
             {!active && (
-              <PrimaryButton onClick={() => openModal()}>
-                Connect wallet
-              </PrimaryButton>
+              <PrimaryButton onClick={openModal}>Connect wallet</PrimaryButton>
             )}
 
-            {active && isAllowListed && (
-              <PrimaryButton onClick={handleClick}>Confirm mint</PrimaryButton>
+            {active && canMint && (
+              <PrimaryButton onClick={handleMint}>Confirm mint</PrimaryButton>
             )}
 
-            {active && !isAllowListed && (
+            {active && !canMint && (
               <PrimaryButton disabled>
                 Not currently eligible for mint.
               </PrimaryButton>
--- src/elf/hooks/useProof.ts
+++ src/elf/hooks/useProof.ts
@@ -1,23 +1,43 @@
+import axios from "axios";
 import { BigNumber, BytesLike } from "ethers";
 import { useQuery } from "react-query";
 
-interface ProofData {
-  tokenId?: BigNumber;
-  proof?: BytesLike[];
-}
-
-const fakeProof = {
-  tokenId: BigNumber.from(0),
-  proof: [
-    "0x3f68e79174daf15b50e15833babc8eb7743e730bb9606f922c48e95314c3905c",
-    "0x31403139b3e90fd160d993560f6de598174a3c5cbb1dd8614454219f590c1d57",
-    "0x2c70357a690b685ecef91703e08c73f3be5a8fe584b65cb1de18eb9dd3308dcd",
-  ],
+type TokenId = BigNumber;
+type Proof = BytesLike[];
+type Leaf = {
+  address: string;
+  tokenId: TokenId;
 };
+type ProofData = { leaf: Leaf; proof: Proof };
+type ProofDataResponse = Array<ProofData>;
 
-// Mocked data for now
+// const fakeProof = {
+//   tokenId: BigNumber.from(0),
+//   proof: [
+//     "0x3f68e79174daf15b50e15833babc8eb7743e730bb9606f922c48e95314c3905c",
+//     "0x31403139b3e90fd160d993560f6de598174a3c5cbb1dd8614454219f590c1d57",
+//     "0x2c70357a690b685ecef91703e08c73f3be5a8fe584b65cb1de18eb9dd3308dcd",
+//   ],
+// };
+
+const S3_BUCKET_URI = "https://elementfi.s3.us-east-2.amazonaws.com/nft";
+
+const getProofURI = (address: string) => `${S3_BUCKET_URI}/goerli2/${address}`;
+
 export const useProof = (address: string | undefined | null) => {
-  return useQuery<ProofData>(["nft-proof", address], () => {
-    return Promise.resolve(fakeProof);
+  return useQuery<ProofData | undefined>(["nft-proof", address], async () => {
+    if (!address) {
+      return undefined;
+    }
+    // console.log(getProofURI(address));
+    const { data } = await axios.get<ProofDataResponse>(getProofURI(address));
+
+    console.log(data);
+
+    if (data && data[0]) {
+      return data[0];
+    }
+
+    return undefined;
   });
 };
