c7581754b916e073444288eaf9bb7c1876dce179
Cash DeLeon <cashd@users.noreply.github.com>
2022-03-06 18:48:52 -0600

    collection no elf state

--- components/Collection/Collection.tsx
+++ components/Collection/Collection.tsx
@@ -1,11 +1,27 @@
+import { PrimaryButton } from "common/Button/styles";
+import { Spacer } from "common/Spacer";
 import { ContentPage } from "components/ContentPage";
-import React from "react";
+import DefconZero from "components/Text/DefconZero";
+import { useHasMinted } from "elf/hooks/useHasMinted";
+import { useWalletDialog } from "elf/hooks/useWalletDialog";
+import useWeb3 from "elf/useWeb3";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 import { Fade } from "react-awesome-reveal";
 import ReactTextTransition, { presets } from "react-text-transition";
 import { CollectionCard } from "./CollectionCard";
 import { CollectionContainer } from "./styles";
 
 export const Collection = () => {
+  const hasMinted = useHasMinted();
+  const { active } = useWeb3();
+  const { open } = useWalletDialog();
+  const { push } = useRouter();
+
+  useEffect(() => {
+    !active && open();
+  }, [active]);
+
   return (
     <ContentPage padding="100px 124px 144px 124px" title="Collection">
       <CollectionContainer>
@@ -16,7 +32,21 @@ export const Collection = () => {
           />
         </h1>
         <Fade>
-          <CollectionCard />
+          {hasMinted ? (
+            <CollectionCard />
+          ) : (
+            <>
+              <DefconZero color="greenLight">
+                No ELF has been minted for this account.
+              </DefconZero>
+
+              <Spacer size="60px" />
+
+              <PrimaryButton onClick={() => push("/mint")}>
+                Confirm eligibility
+              </PrimaryButton>
+            </>
+          )}
         </Fade>
       </CollectionContainer>
     </ContentPage>
