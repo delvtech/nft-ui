7aa595c2b2ad4e0dcb200d242120210c6f46a9ab
Cash DeLeon <cashd@users.noreply.github.com>
2022-03-08 11:53:53 -0600

    useEagerConnect hook
    copied from core app

--- components/Wallet/WalletNotifier.tsx
+++ components/Wallet/WalletNotifier.tsx
@@ -1,3 +1,4 @@
+import { useEagerConnect } from "elf/hooks/useEagerConnect";
 import useWeb3 from "elf/useWeb3";
 import { createToastSuccess } from "helpers/createToast";
 import { WithChildren } from "helpers/types";
@@ -6,6 +7,8 @@ import React, { useEffect } from "react";
 // Higher order component for wallet events, mounts toast notifications.
 export const WalletNotifier = ({ children }: WithChildren) => {
   const { account } = useWeb3();
+
+  useEagerConnect();
 
   // Add use effect hooks for more event coverage
   useEffect(() => {
