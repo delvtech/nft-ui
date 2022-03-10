3ffc984e7860a27cc8028efc5ddefbecc5df1e3b
Cash DeLeon <cashd@users.noreply.github.com>
2022-03-10 16:06:55 -0600

    nullable address type update

--- src/elf/hooks/useTokenBalanceOf.ts
+++ src/elf/hooks/useTokenBalanceOf.ts
@@ -1,10 +1,11 @@
 import { useSmartContractReadCall } from "@elementfi/react-query-typechain";
 import useWeb3 from "elf/useWeb3";
+import { NullableAddress } from "src/types";
 import { ElfNFT__factory } from "typechain-types";
 import { useAddresses } from "./useAddress";
 import { useProvider } from "./useProvider";
 
-export function useTokenBalanceOf(address: string | null | undefined) {
+export function useTokenBalanceOf(address: NullableAddress) {
   const { chainId } = useWeb3();
 
   const addresses = useAddresses();
