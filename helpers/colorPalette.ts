0fd918dc73e8ae3420ebce0d98b15742026b1ab8
Cash DeLeon <cashd@users.noreply.github.com>
2022-03-28 17:47:35 -0500

    Update colorPalette.ts

--- helpers/colorPalette.ts
+++ helpers/colorPalette.ts
@@ -12,6 +12,7 @@ export const COLOR_TRANSPARENT = "transparent";
 export const COLOR_SEMI_TRANSPARENT = "rgba(0, 0, 0, 0.2)";
 export const COLOR_LIGHT_GRAY = "#999EA1";
 export const COLOR_GRAY_LIGHT = "#D9DBE9";
+export const COLOR_JADE = "#1A535C";
 
 export const COLORS = {
   black: COLOR_BLACK,
@@ -28,6 +29,7 @@ export const COLORS = {
   yellow: COLOR_YELLOW,
   semiTransparent: COLOR_SEMI_TRANSPARENT,
   grayLight: COLOR_GRAY_LIGHT,
+  jade: COLOR_JADE,
 } as const;
 
 export type Color = keyof typeof COLORS;
