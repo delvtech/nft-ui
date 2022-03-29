a81f73584c98c68092a77524932e39b2e7364aa9
Cash DeLeon <cashd@users.noreply.github.com>
2022-03-29 14:45:08 -0500

    Update useTokenBalanceOf.ts

--- src/elf/hooks/useTokenBalanceOf.ts
+++ src/elf/hooks/useTokenBalanceOf.ts
@@ -1,13 +1,10 @@
 import { useSmartContractReadCall } from "@elementfi/react-query-typechain";
 import { getProvider } from "elf/providers";
-import useWeb3 from "elf/useWeb3";
 import { NullableAddress } from "src/types";
 import { ElfNFT__factory } from "typechain-types";
 import { useAddresses } from "./useAddress";
 
 export function useTokenBalanceOf(address: NullableAddress) {
-  const { chainId } = useWeb3();
-
   const addresses = useAddresses();
   const provider = getProvider();
 
