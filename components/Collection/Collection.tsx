fa0fff66426c6550838bf738c57d55f38e8804d6
Cash DeLeon <cashd@users.noreply.github.com>
2022-03-30 03:02:30 -0500

    card component, styled comonpent renames, and hover shadow

--- components/Collection/Collection.tsx
+++ components/Collection/Collection.tsx
@@ -1,50 +1,18 @@
 import { Spacer } from "common/Spacer";
 import DefconZero from "components/Text/DefconZero";
-import { useHasMinted } from "elf/hooks/useHasMinted";
-import { useMintDate } from "elf/hooks/useMintDate";
 import { useTokenIds } from "elf/hooks/useTokenIds";
 import { useWalletDialog } from "elf/hooks/useWalletDialog";
 import useWeb3 from "elf/useWeb3";
-import {
-  COLORS,
-  COLOR_GREEN_LIGHT,
-  COLOR_TRANSPARENT,
-  COLOR_WHITE_LIGHT,
-} from "helpers/colorPalette";
+import { COLORS } from "helpers/colorPalette";
 import { devices } from "helpers/devices";
 import Image from "next/image";
-import { useRouter } from "next/router";
-import React, { PropsWithChildren, useEffect } from "react";
+import React, { useEffect } from "react";
 import { DayCount } from "src/types";
 import { getTokenAssetURL } from "src/urls";
 import styled from "styled-components";
+import Card from "./Card";
 import { MintHistoryChart } from "./MintHistoryChart";
 import { MintingPeriodStatus } from "./MintingPeriodStatus";
-
-const Card = ({ title, children }: PropsWithChildren<{ title: string }>) => (
-  <CardContainer>
-    <h3>{title}</h3>
-    <Spacer size="4px" />
-    <svg width="100%" height={1}>
-      <line x1="0" y1="0" x2="1000" y2="0" stroke={COLORS.whiteLight} />
-    </svg>
-    {children}
-    <Spacer size="4px" />
-  </CardContainer>
-);
-
-const CardContainer = styled.div`
-  background-color: rgba(247, 255, 247, 0.07);
-
-  padding: 24px;
-  margin: 5px;
-
-  border-radius: 10px;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  width: fit-content;
-`;
 
 interface CollectionProps {
   mintHistory: Array<DayCount>;
@@ -55,13 +23,10 @@ export const Collection = ({
   mintHistory,
   delegationHistory,
 }: CollectionProps) => {
-  const hasMinted = useHasMinted();
   const { active, account } = useWeb3();
   const { open, close } = useWalletDialog();
-  const { push } = useRouter();
   const tokenIds = useTokenIds(account);
   const firstTokenId = tokenIds[0];
-  const { data: mintDate } = useMintDate(firstTokenId);
 
   const tokenURL = getTokenAssetURL(firstTokenId);
 
@@ -75,27 +40,20 @@ export const Collection = ({
 
   return (
     <ContentPageContainer>
-      <CollectionContainer>
-        <h1>My ELF Collection</h1>
-        <BodyContainer>
-          <GraphContainer>
-            <Card title="Minting Inventory">
-              <MintingPeriodStatus totalMints={delegationHistory.length} />
-            </Card>
-            <Card title="Minting History">
-              <MintHistoryChart mintHistory={mintHistory} />
-            </Card>
-          </GraphContainer>
-
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              overflow: "scroll",
-            }}
-          >
-            <Card title="Claimed ELF">
-              {false ? (
+      <Header>My ELF Collection</Header>
+      <Body>
+        <GraphContainer>
+          <Card title="Minting Inventory">
+            <MintingPeriodStatus totalMints={delegationHistory.length} />
+          </Card>
+          <Card title="Minting History">
+            <MintHistoryChart mintHistory={mintHistory} />
+          </Card>
+        </GraphContainer>
+        <ClaimElfContainer>
+          <Card title="Claimed ELF">
+            {!!tokenIds.length ? (
+              tokenURL && tokenURL?.length > 2 ? (
                 <ElfContainer>
                   <Image
                     src={tokenURL}
@@ -108,36 +66,41 @@ export const Collection = ({
                   <DefconZero size="16px">ELF 123</DefconZero>
                 </ElfContainer>
               ) : (
-                <_TokensContainer>
-                  {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map(
-                    (x) => (
-                      <ElfContainer>
-                        {tokenURL ? (
-                          <Image
-                            src={tokenURL}
-                            height={200}
-                            width={200}
-                            alt="Minted elf"
-                            quality={100}
-                          />
-                        ) : (
-                          <ImagePlaceholder />
-                        )}
-                        <Spacer size="6px" />
+                <Tokens>
+                  {tokenIds.map((id) => (
+                    <ElfContainer>
+                      {tokenURL ? (
+                        <Image
+                          src={tokenURL}
+                          height={200}
+                          width={200}
+                          alt="Minted elf"
+                          quality={100}
+                        />
+                      ) : (
+                        <ImagePlaceholder />
+                      )}
+                      <Spacer size="6px" />
 
-                        <DefconZero size="16px">ELF 123</DefconZero>
-                      </ElfContainer>
-                    ),
-                  )}
-                </_TokensContainer>
-              )}
-            </Card>
-          </div>
-        </BodyContainer>
-      </CollectionContainer>
+                      <DefconZero size="16px">ELF 123</DefconZero>
+                    </ElfContainer>
+                  ))}
+                </Tokens>
+              )
+            ) : (
+              <ImagePlaceholder />
+            )}
+          </Card>
+        </ClaimElfContainer>
+      </Body>
     </ContentPageContainer>
   );
 };
+
+const Header = styled.text`
+  font-size 50px;
+  font-family: Defcon Zero;
+`;
 
 const GraphContainer = styled.div`
   display: flex;
@@ -145,42 +108,30 @@ const GraphContainer = styled.div`
 
   @media ${devices.desktopL} {
     flex-direction: column;
+  }
 `;
 
-const BodyContainer = styled.div`
+const Body = styled.div`
   display: flex;
   flex-wrap: wrap;
-
   padding: 10px;
   border-radius: 20px;
   justify-content: center;
+  margin-top: 30px;
 `;
 
-const CollectionContainer = styled.div`
-  h3 {
-    font-size: 14px;
-    font-family: Rubik;
-
-    place-self: flex-start;
-    margin-bottom: 10px;
-  }
-
-  // * {
-  //   outline: 1px solid red;
-  // }
+const ClaimElfContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow: "scroll";
 `;
 
-const _TokensContainer = styled.div`
+const Tokens = styled.div`
   display: grid;
   grid-template-rows: auto;
   grid-template-columns: 200px 200px;
-
   max-height: 600px;
   overflow: scroll;
-
-  @media ${devices.tabletL} {
-    //grid-template-columns: 200px;
-  }
 
   @media ${devices.tabletM} {
     grid-template-columns: 200px 200px;
@@ -193,15 +144,21 @@ const ElfContainer = styled.div`
   height: min-content;
   align-items: center;
   padding: 12px;
-
   margin: 10px;
-
   background-color: ${COLORS.greenDarkest};
   border-radius: 14px;
+
+  box-shadow: 0px 0px 0px rgba(20, 223, 186, 0.8);
+
+  transition: 0.3s;
 
   div,
   img {
     border-radius: 12px 12px 0px 0px;
+  }
+
+  :hover {
+    box-shadow: 0px 5px 20px rgba(20, 223, 186, 0.8);
   }
 `;
 
@@ -211,15 +168,26 @@ const ImagePlaceholder = styled.div`
   background: ${COLORS.semiTransparent};
 `;
 
-const ContentPageContainer = styled.div<{
-  padding?: string;
-}>`
-  padding: ${({ padding }) => padding ?? "100px 0px 235px 0px"};
+const ContentPageContainer = styled.div`
+  // * {
+  //   outline: 1px solid red;
+  // }
+
+  padding: 100px 0px 0px 0px;
   background-color: #09282d;
-  border: 3px solid ${COLOR_WHITE_LIGHT};
+  border: 3px solid ${COLORS.whiteLight};
+  margin-top: 30px;
 
   h1 {
     font-size: 3rem;
+  }
+
+  h3 {
+    font-size: 14px;
+    font-family: Rubik;
+
+    place-self: flex-start;
+    margin-bottom: 10px;
   }
 
   br {
@@ -235,15 +203,16 @@ const ContentPageContainer = styled.div<{
   }
 
   mark {
-    color: ${COLOR_GREEN_LIGHT};
-    background: ${COLOR_TRANSPARENT};
+    color: ${COLORS.greenLight};
+    background: ${COLORS.transparent};
   }
 
   @media ${devices.tabletL} {
-    padding: 80px 65px 155px 65px;
+    padding: 80px 0px 80px 0px;
+    margin-top: 50px;
   }
 
   @media ${devices.mobileL} {
-    padding: 60px 1rem 125px 1rem;
-  } ;
+    padding: 30px 0px 30px 0px;
+  }
 `;
