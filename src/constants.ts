c4fe7ab3584ef71817206c75f98cf41504ce854a
tina <tinahaibodi96@gmail.com>
2022-04-05 17:09:21 +0200

    fixing format of release date to be relative and be launch time (#58)
    * fixing format of release date to be relative and be launch time
    
    * Update constants.ts
    
    * Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-export const releaseDate = moment("2022-04-07");
+export const releaseDate = moment("2022-04-06T16:00:00+00:00"); // April 6th, 6:00 pm EST
 
 export const firstBatchMintMax = 2000;
 export const TOS_LOCAL_KEY = "Elfiverse-TOS-Acceptance";
