82b0ae5bfd9b79e21dd4fe37e35fde23d471ee5e
tinahaibodi <27844500+tinahaibodi@users.noreply.github.com>
2022-04-05 15:46:16 +0200

    changing date for UTC community members

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-export const releaseDate = moment("2022-04-06");
+export const releaseDate = moment("2022-04-07");
 
 export const firstBatchMintMax = 2000;
 export const TOS_LOCAL_KEY = "Elfiverse-TOS-Acceptance";
