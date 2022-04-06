3b58718f6230b54ada3dbcec5e904b10ab0905b0
Cash Baller <cashd@users.noreply.github.com>
2022-04-06 17:23:54 -0500

    stop using useAddresses hook (#69)

--- src/elf/hooks/useOwnerOf.ts
+++ src/elf/hooks/useOwnerOf.ts
@@ -2,13 +2,13 @@ import { useSmartContractReadCall } from "@elementfi/react-query-typechain";
 import { getProvider } from "elf/providers";
 import useWeb3 from "elf/useWeb3";
 import { BigNumber } from "ethers";
+import { getAddresses } from "src/addresses";
 import { ElfNFT__factory } from "typechain-types";
-import { useAddresses } from "./useAddress";
 
 export function useOwnerOf(tokenId: BigNumber | undefined) {
   const { chainId } = useWeb3();
 
-  const addresses = useAddresses();
+  const addresses = getAddresses();
   const provider = getProvider();
 
   const tokenContract = ElfNFT__factory.connect(
